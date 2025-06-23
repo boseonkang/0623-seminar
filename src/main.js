e262c6b36e2e584ff9a5a81bea89c0acda1bb358
boseonkang <qhtjs34@snu.ac.kr>
2025-06-23 11:39:04 +0900

    수정함
    대충 창을 더 멋지게 sweetalert2사용

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-document.getElementById('warning-button').addEventListener('click', () => {
-  alert('I Said Do Not Click This Button');
+import Swal from 'sweetalert2';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById('warning-button');
+  button.addEventListener('click', () => {
+    Swal.fire({
+      title: '⚠️ I Said Do Not Click!',
+      text: 'You were warned...',
+      icon: 'warning',
+      confirmButtonText: 'Sorry 😓',
+      background: '#e3f2fd',
+      color: '#0d47a1',
+      confirmButtonColor: '#64b5f6',
+    });
+  });
 });
